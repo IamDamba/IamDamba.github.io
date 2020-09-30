00d5ce88dd1682fce481ea867ec9a4502fccc10f
IamDamba <jordi.damba.jd@gmail.com>
2020-09-30 22:33:23 +0200

    Updates

---
+++ precache-manifest.4d73cb94bd22f1f53a9f6c9f0e7e8e66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfdd095205d4a95a8d93bce73e9101c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a7c5cc6c725be3eb99",
+    "url": "/static/css/main.42ea8ec0.chunk.css"
+  },
+  {
+    "revision": "9ab222ecb51bf495964b",
+    "url": "/static/js/2.787958fa.chunk.js"
+  },
+  {
+    "revision": "2c0b89fe32f6d0b561deea7159cbd810",
+    "url": "/static/js/2.787958fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a7c5cc6c725be3eb99",
+    "url": "/static/js/main.6a5e59a6.chunk.js"
+  },
+  {
+    "revision": "7290da71940e29d2e647",
+    "url": "/static/js/runtime-main.6ec53615.js"
+  },
+  {
+    "revision": "d4e331a52aa4f4a426624afb32e75dc1",
+    "url": "/static/media/CV.d4e331a5.pdf"
+  },
+  {
+    "revision": "d0ceb9f6d40d11222a97a28b623d2f1c",
+    "url": "/static/media/IAMDAMBA.d0ceb9f6.svg"
+  }
+]);
